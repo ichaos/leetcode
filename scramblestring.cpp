62f7a708aa70a1458de966d61afef5a66b00c743
Jin Chen <ciccinocj@gmail.com>
2013-05-31 22:28:42 +0800

    using dynamic programming to solve scramble string problem

--- scramblestring.cpp
+++ scramblestring.cpp
@@ -5,55 +5,31 @@ class Solution {
     private:
 public:
     bool isScramble(string s1, string s2) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (s1.length() != s2.length()) return false;
-    
         int n = s1.length();
-        bool find = false;
-        int dif = -1;
+        if (s2.length() != n) return false;
+
+        int f[n][n][n];
+
         for (int i = 0; i < n; i++) {
-            if (s1[i] != s2[i]) {
-                find = true;
-                dif = i;
-                break;
+            for (int j = 0; j < n; j++) {
+                f[i][j][0] = (s1[i] == s2[j]);
             }
         }
-        if (!find) return true;
-        int mid = dif;
-        find = false;
-        for (int i = dif + 1; i < n; i++) {
-            if (s1[dif] == s2[i]) {
-                mid = i;
-                find = true;
-                break;
+
+        for (int l = 1; l < n; l++) {
+            for (int i = 0; i < n - l; i++) {
+                for (int j = 0; j < n - l; j++) {
+                    f[i][j][l] = false;
+                    for (int k = 0; k < l; k++) {
+                        if ((f[i][j][k] && f[i + k + 1][j + k + 1][l - k - 1]) ||
+                            (f[i][j + l - k][k] && f[i + k + 1][j][l - k - 1])) {
+                            f[i][j][l] = true;
+                            break;
+                        }
+                    }
+                }
             }
         }
-        if (!find) return false;
-        int end = n;
-        find = false;
-        for (int i = 1; i + mid < n; i++) {
-            if (s1[i + dif] != s2[i + mid]) {
-                end = i + mid;
-                find = true;
-                break;
-            }
-        }
-        
-        //check s1[dif...] === s2[mid...end - 1] && s1[dif + end - mid...] = s2[dif...mid - 1]
-        for (int i = 1; i < end - mid; i++) {
-            if (s1[i + dif] != s2[mid + i]) {
-                return false;
-            }
-        }
-        for (int i = 1; i < mid - dif; i++) {
-            if (s2[i + dif] != s1[dif + end - mid + i]) {
-                return false;
-            }
-        }
-        for (int i = end; i < n; i++) {
-            if (s1[i] != s2[i]) return false;
-        }
-        return true;
+        return f[0][0][n - 1];
     }
 };
