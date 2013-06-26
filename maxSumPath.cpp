30f45527a3d8fd2f6e115134aaa4a087c1713e78
Jin Chen <ciccinocj@gmail.com>
2013-06-26 21:14:45 +0800

    second version of maxSumPath, better performance

--- maxSumPath.cpp
+++ maxSumPath.cpp
@@ -9,38 +9,41 @@
  */
 class Solution {
 private:
-    int maxSum(TreeNode *root) {
+    int maxSum(TreeNode *root, int *maxChild) {
+        int lm = 0;
+        int rm = 0;
+        
+        *maxChild = 0;
         if (!root) return 0;
-        int a = max(maxSum(root->left), maxSum(root->right));
+        int left = maxSum(root->left, &lm);
+        int right = maxSum(root->right, &rm);
+        
+        int a = max(lm, rm);
+        *maxChild = root->val;
+        if (a > 0)
+            *maxChild += a;
+        
+        a = 0;
+        if (lm > 0)
+            a += lm;
+        if (rm > 0)
+            a += rm;
         a += root->val;
+        
+        if (root->left)
+            a = max(a, left);
+        if (root->right)
+            a = max(a, right);
+        
         return a;
-        if (a > 0) return a;
-        else return 0;
     }
 public:
     int maxPathSum(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (!root) return 0;
-        int a = maxPathSum(root->left);
-        int b = maxPathSum(root->right);
-        
-        //assume no negative value
-        //int c = maxSum(root->left) + maxSum(root->right) + root->val;
+        //if (!root) return 0;
         int c = 0;
-        ///*
-        int tmp = maxSum(root->left);
-        if (tmp > 0)
-            c += tmp;
-        tmp = maxSum(root->right);
-        if (tmp > 0)
-            c += tmp;
-        c += root->val;
-        //*/
-        if (root->right)
-            a = max(a, b);
-        if (root->left)
-            c = max(a, c);
-        return c;
+        int r = maxSum(root, &c);
+        return r;
     }
 };
