37342984a2deee91205cb42ab1426081943d8dd5
Jin Chen <ciccinocj@gmail.com>
2013-06-27 23:18:14 +0800

    Buy and sell stock to find maximum profit. II

---
+++ buySellStockII.cpp
@@ -0,0 +1,27 @@
+/**
+ * Best time to buy and sell stock II
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. 
+ * You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). 
+ * However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ */
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (prices.size() < 2) return 0;
+        
+        int buy = prices[0];
+        int sum = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > buy) {
+                sum += (prices[i] - buy);
+            }
+            buy = prices[i];
+        }
+        return sum;
+    }
+};
