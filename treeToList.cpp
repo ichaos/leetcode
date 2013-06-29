2b9c2c0b8284ee93827d74563fd3f3535e432866
Jin Chen <ciccinocj@gmail.com>
2013-06-30 00:08:02 +0800

    Flatten Binary Tree to Linked List

--- treeToList.cpp
+++ treeToList.cpp
@@ -1,24 +1,47 @@
-#include <iostream>
-#include <stdio.h>
-#include <stack>
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
 
-using namespace std;
+For example,
+Given
 
-struct TreeNode {
-     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-
-	void flatten(TreeNode *root) {
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+Hints:
+If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+ *
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         if (!root) return;
+        //return;
 
         stack<TreeNode *> s;
-	    TreeNode *list = root;
+        TreeNode *list = root;
 	    TreeNode *cur;
 	    if (root->right) s.push(root->right);
 	    if (root->left) s.push(root->left);
@@ -28,20 +51,10 @@ struct TreeNode {
 		    cur = s.top();
 		    s.pop();
 		    list->right = cur;
+            list->left = NULL;
 		    list = cur;
 		    if (cur->right) s.push(cur->right);
 		    if (cur->left) s.push(cur->left);
 	    }
     }
-
-int main() {
-	TreeNode root(1);
-	TreeNode right(2);
-	root.right = &right;
-	flatten(&root);
-	TreeNode *cur = &root;
-	while (cur) {
-		cout << "cur->val: " << cur->val << endl;
-		cur = cur->right;
-	}
-}
+};
