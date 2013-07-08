e86343b8a291481bab5afc78f1509b9e0d95d8aa
Jin Chen <ciccinocj@gmail.com>
2013-07-08 21:03:46 +0800

    Sunday string matching algorithm

--- strStr.cpp
+++ strStr.cpp
@@ -6,24 +6,46 @@ Returns a pointer to the first occurrence of needle in haystack, or null if need
  */
 class Solution {
 private:
-    bool isEqual(char *a, char *b) {
-        while (*b != '\0') {
-            if (*a++ != *b++) return false;
+    int findSlip(char *end, int l, char c) {
+        int r = l;
+        while (c != *end && r > 0) {
+            r--;
+            end--;
         }
-        return true;
+        return l - r + 1;
     }
 public:
     char *strStr(char *haystack, char *needle) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (needle == NULL || haystack == NULL) return NULL;
-        if (*needle == '\0') return haystack;
-        char *h = haystack;
-        while (*h != '\0') {
-            if (isEqual(h, needle))
-                return h;
-            h++;
+        //sunday
+        char *hp = haystack;
+        char *np = needle;
+        
+        int l = 0;
+        while (*np != '\0') {
+            l++;
+            np++;
         }
+        if (l == 0) return hp;
+        np = needle;
+        int i = 0;
+        while (*hp != '\0') {
+            if (*np == '\0') {
+                return hp;
+            }
+            if (*(hp + i) == '\0') return NULL;
+            if (*(hp + i) == *np) {
+                i++;
+                np++;
+            } else {
+                int step = findSlip(needle + l - 1, l, *(hp + l));
+                hp += step;
+                np = needle;
+                i = 0;
+            }
+        }
+
         return NULL;
     }
 };
