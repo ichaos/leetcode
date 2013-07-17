759c4e6398150988c25de13a1231799a7d651008
Jin Chen <ciccinocj@gmail.com>
2013-07-17 21:43:16 +0800

    Wildcard Matching version 2 - depth first searchl

--- wildcardMatching.cpp
+++ wildcardMatching.cpp
@@ -19,29 +19,53 @@ isMatch("ab", "?*") ? true
 isMatch("aab", "c*a*b") ? false
  */
 class Solution {
+private:
+    struct wildcard {
+        const char *s;
+        const char *p;
+        wildcard(const char *ss, const char *pp) : s(ss), p(pp) {}
+    };
 public:
     bool isMatch(const char *s, const char *p) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (s[0] == '\0' && p[0] == '\0') return true;
+        stack<wildcard> stk;
         
-        if (p[0] == '?') {
-            if (s[0] == '\0') return false;
-            return isMatch(s + 1, p + 1);
-        }
-        if (p[0] == '*') {
-            if (s[0] == '\0') {
-                if (isMatch(s, p + 1)) return true;
-                return false;
-            } else {
-                if (isMatch(s, p + 1) || isMatch(s + 1, p + 1) ||
-                    isMatch(s + 1, p))
-                    return true;
-                else 
-                    return false;
+        wildcard wc(s, p);
+        stk.push(wc);
+        
+        while (!stk.empty()) {
+            wildcard wc = stk.top();
+            stk.pop();
+            
+            if (wc.s[0] == '\0' && wc.p[0] == '\0') return true;
+            
+            if (wc.p[0] == '?') {
+                if (wc.s[0] != '\0') {
+                    wc.s++;
+                    wc.p++;
+                    stk.push(wc);
+                }
+            } else if (wc.p[0] == '*') {
+                if (wc.s[0] == '\0') {
+                    wc.p++;
+                    stk.push(wc);
+                } else {
+                    wildcard t1(wc.s, wc.p + 1);
+                    stk.push(t1);
+                    t1.s = wc.s + 1;
+                    t1.p = wc.p;
+                    stk.push(t1);
+                    t1.s = wc.s + 1;
+                    t1.p = wc.p + 1;
+                    stk.push(t1);
+                }
+            } else if (wc.s[0] == wc.p[0]) {
+                wc.s++;
+                wc.p++;
+                stk.push(wc);
             }
         }
-        if (s[0] == p[0]) return isMatch(s + 1, p + 1);
         return false;
     }
 };
