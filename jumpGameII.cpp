e90c5474a05d208e537ff9e5c39689714101a1ff
Jin Chen <ciccinocj@gmail.com>
2013-07-18 21:16:33 +0800

    Jump Game -- greedy algorithm

--- jumpGameII.cpp
+++ jumpGameII.cpp
@@ -11,24 +11,23 @@ Given array A = [2,3,1,1,4]
 
 The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
  */
-class Solution {
+class Solution {    
 public:
     int jump(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (n < 1) return 0;
+        int d = 0;
+        int last = 0;
+        int cur = 0;
         
-        int B[n];
         for (int i = 0; i < n; i++) {
-            B[i] = INT_MAX - 10;
-        }
-        B[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= A[i] && i + j < n; j++) {
-                if (B[i + j] - 1 > B[i])
-                    B[i + j] = B[i] + 1;
+            if (i > last) {
+                last = cur;
+                d++;
             }
+            if (cur < i + A[i]) cur = i + A[i];
         }
-        return B[n - 1];
+        
+        return d;
     }
 };
