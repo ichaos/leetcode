3dc6ab758c81fd2b7a98fde0354a3e3640eaea1f
Jin Chen <ciccinocj@gmail.com>
2013-07-19 20:26:01 +0800

    anagrams -- fast version

--- anagrams.cpp
+++ anagrams.cpp
@@ -7,38 +7,22 @@ Note: All inputs will be in lower-case.
  */
 
 class Solution {
-private:
-    bool areAnagrams(string a, string b) {
-        sort(b.begin(), b.end());
-        sort(a.begin(), a.end());
-        return a == b;
-    }
 public:
     vector<string> anagrams(vector<string> &strs) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        map<string, vector<string> > ht;
         vector<string> v;
-        vector<string> depot(strs);
-        if (strs.size() <= 1) return v;
+        for (int i = 0; i < strs.size(); i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            ht[s].push_back(strs[i]);
+        }
         
-        string prev;
-        bool anagram = false;
-        for (int i = 0; i < depot.size(); i++) {
-            prev = depot[i];
-            anagram = false;
-            for (int j = i + 1; j < depot.size(); j++) {
-                if (areAnagrams(prev, depot[j])) {
-                    //v.push_back(prev);
-                    v.push_back(depot[j]);
-                    depot.erase(depot.begin() + j);
-                    j--;
-                    anagram = true;
-                }
-            }
-            if (anagram) {
-                v.push_back(prev);
-                depot.erase(depot.begin() + i);
-                i--;
+        map<string, vector<string> >::iterator it;
+        for (it = ht.begin(); it != ht.end(); it++) {
+            if (it->second.size() > 1) {
+                v.insert(v.begin(), it->second.begin(), it->second.end());
             }
         }
         return v;
