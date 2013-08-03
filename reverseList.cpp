65f0e8b42cfb5ca516e01af13ba7be855b0b0881
Jin Chen <ciccinocj@gmail.com>
2013-08-03 19:32:14 +0800

    Reverse List

--- reverseList.cpp
+++ reverseList.cpp
@@ -1,4 +1,14 @@
 /**
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ? m ? n ? length of list.
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -27,7 +37,7 @@ public:
         while (cur) {
             i++;
             tmp = cur->next;
-            if (i >= n) {
+            if (i > n) {
                 break;
             }
             if (i == m - 1) { //prepare
